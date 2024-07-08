66d25bd4f8ab4bc5e11e99367e9650d87a7667d2
Sam Mercer <smercer10@proton.me>
2024-07-08 23:36:17 +0100

    Process command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("The target application must be provided as an argument.");
+        std::process::exit(1);
+    }
+
+    let _debuggee = std::process::Command::new(&args[1])
+        .spawn()
+        .expect("Failed to start the target application.");
 }
