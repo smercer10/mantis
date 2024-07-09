6cd154937b36c81a36c905f8517df9e9f52c610a
Sam Mercer <smercer10@proton.me>
2024-07-09 19:00:28 +0100

    Use nix to create and trace child process

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::env;
+use nix::{
+    sys::ptrace,
+    unistd::{fork, ForkResult},
+};
+use std::{env, os::unix::process::CommandExt, process::Command};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,7 +12,24 @@ fn main() {
         std::process::exit(1);
     }
 
-    let _debuggee = std::process::Command::new(&args[1])
-        .spawn()
-        .expect("Failed to start the target application.");
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child }) => {
+            println!("Debugger started. Child PID: {}", child);
+        }
+        Ok(ForkResult::Child) => {
+            if let Err(err) = ptrace::traceme() {
+                eprintln!("Failed to trace child process: {}", err);
+                std::process::exit(1);
+            }
+
+            Command::new(&args[1]).args(&args[2..]).exec();
+
+            // This line should never be reached if the exec call is successful.
+            eprintln!("Failed to execute the target application.");
+        }
+        Err(_) => {
+            eprintln!("Failed to fork the process.");
+            std::process::exit(1);
+        }
+    }
 }
